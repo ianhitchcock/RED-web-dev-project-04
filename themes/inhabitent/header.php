d8732bc165e1288dfe0c70e22ec2b7d3742b09d9
Ian <ianhitchcock@outlook.com>
2017-11-22 17:01:29 -0800

    header fix for admin 🙃

--- themes/inhabitent/header.php
+++ themes/inhabitent/header.php
@@ -22,11 +22,11 @@
 		body_class( "page" );
 	} else {
 		body_class();
-	} ?>>
+	}?>>
 		<div id="page" class="hfeed site">
 			<a class="skip-link screen-reader-text" href="#content"><?php esc_html( 'Skip to content' ); ?></a>
 
-			<header id="masthead" class="site-header" role="banner">
+			<header id="masthead" class="site-header <?php if ( is_admin_bar_showing() ) { echo "admin-bar";}?>" role="banner">
 				<div class="container">
 					<div class="site-branding">
 						<h1 class="site-title screen-reader-text"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
